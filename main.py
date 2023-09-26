0098b94bebf890db2712cdd04a796dad08927c4a
GGal1leo <g4l1le0@protonmail.ch>
2023-09-26 22:20:21 +0100

    update

--- main.py
+++ main.py
@@ -39,18 +39,78 @@ def decode(message):
         # convert the binary representation to a character
         clear += chr(int(binary, 2))
     return clear
+
+def wrapEncode(message):
+    # wrap the encoded message in another message
+    encoded = encode(message)
+    wrapper = input('Enter the message you want to wrap the encoded message in: ')
+    if len(wrapper) != encoded.count('$')-2:
+        # pad the wrapper with dots
+        while len(wrapper) <= encoded.count('$')-2:
+            wrapper += '.'
+    wrapped = ''
+    bits = encoded.split('$')
+    # remove first element, which is an empty string
+    bits.pop(0)
+    # remove last 2 elements, which are an empty string
+    bits.pop()
+    bits.pop()
+    # print(len(bits))
+    # print(bits)
+    for i in range(len(bits)):
+        # print(wrapper[i])
+        wrapped += wrapper[i] + bits[i]
+    return wrapped + wrapper[len(wrapped.replace('\u200b', '').replace('\u200c', '')):] 
     
+def wrapDecode(message):
+    pog = ""
+    temp = []
+    # group the zero width characters each group being the one separated by a non zero width character
+    for i in range(len(message)):
+        if message[i] == '\u200b' or message[i] == '\u200c':
+            pog += message[i]
+        else:
+            temp.append(pog)
+            pog = ''
+    # remove empty elements
+    while '' in temp:
+        temp.remove('')
+    # temp.pop(0)
+    dec = ''
+    for char in temp:
+        binary = ''
+        for i in char:
+            if i == '\u200b':
+                binary += '0'
+            else:
+                binary += '1'
+        # convert the binary representation to a character
+        dec += chr(int(binary, 2))
+    return dec
+
     
 
 choice = input('Would you like to encode or decode a message? (e/d): ')
 if choice == 'e':
-    user_input = input('Enter the message you want to encrypt: ')
-    print(encode(user_input))
+    # ask if they would like to wrap the message in another message
+    wrap = input('Would you like to wrap the encoded message in another message? (y/n): ')
+    if wrap == 'y':
+        user_input = input('Enter the message you want to encrypt and wrap: ')
+        print(wrapEncode(user_input))
+    elif wrap == 'n':
+        user_input = input('Enter the message you want to encrypt: ')
+        print(encode(user_input))
 elif choice == 'd':
-    user_input = input('Enter the message you want to decrypt: ')
-    print(decode(user_input))
+    # ask if they would like to unwrap the message
+    wrap = input('Would you like to unwrap the message? (y/n): ')
+    if wrap == 'y':
+        user_input = input('Enter the message you want to unwrap and decrypt: ')
+        print(wrapDecode(user_input))
+    elif wrap == 'n':
+
+        user_input = input('Enter the message you want to decrypt: ')
+        print(decode(user_input))
 else:
     print('Invalid choice. Exiting...')
     exit()
 
-    
